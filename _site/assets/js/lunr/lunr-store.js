5f0ae8ba3e70060d0c6395caea01a88d8a8d69bd
aleksandarskrbic <skrbic.alexa@gmail.com>
2020-04-04 19:13:17 +0200

    finish first akka blog:

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Data processing with Akka Actors: Part I",
+        "excerpt":"Everyone experienced in a field of distributed and data-intensive systems or big data technologies,in general, must have heard about Akka.This article will be part of the series, where I will try to introduce you to the actor programming model while developing a data processing application exploiting techniques provided by Akka...","categories": [],
+        "tags": ["scala","akka","jvm","big data"],
+        "url": "http://localhost:4000/akka-actors-part-1/",
+        "teaser":null}]
